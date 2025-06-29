df4734e0d5fbc47beef6444e230906f871ae8367
Kirito54 <33008107+Kirito54@users.noreply.github.com>
2025-06-29 21:33:51 +0300

    Convert app to Blazor WebAssembly

--- reestr/Program.cs
+++ reestr/Program.cs
@@ -1,52 +1,29 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using reestr.Components;
 using MudBlazor.Services;
 using reestr.Services;
 
-var builder = WebApplication.CreateBuilder(new WebApplicationOptions
-{
-    ContentRootPath = AppContext.BaseDirectory,
-    Args = args
-});
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
 builder.Services.AddMudServices();
-builder.Services.AddSingleton<ExcelDataService>(sp =>
-{
-    var env = sp.GetRequiredService<IWebHostEnvironment>();
-    var service = new ExcelDataService();
-    var file = Path.Combine(env.WebRootPath, "data.xlsx");
-    if (File.Exists(file))
-    {
-        try
-        {
-            service.LoadFromFile(file);
-        }
-        catch (Exception ex)
-        {
-            sp.GetRequiredService<ILogger<Program>>().LogWarning(ex, "Failed to load Excel data");
-        }
-    }
-    return service;
-});
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<ExcelDataService>();
 
-var app = builder.Build();
+var host = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+var excelService = host.Services.GetRequiredService<ExcelDataService>();
+try
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    using var http = host.Services.GetRequiredService<HttpClient>();
+    using var stream = await http.GetStreamAsync("data.xlsx");
+    excelService.LoadFromStream(stream);
+}
+catch (Exception ex)
+{
+    host.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Program").LogWarning(ex, "Failed to load Excel data");
 }
 
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
+await host.RunAsync();
